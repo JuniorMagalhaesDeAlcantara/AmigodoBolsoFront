4569b127290ab9d92d5d17c24fe72c5b727282fb
Junior Magalhães <jrlevita09@hotmail.com>
2023-05-20 19:02:12 -0300

    Ajuste links e Layout Dicas para o Bolso

--- src/app/dicas-para-o-bolso/dicas-para-o-bolso.component.ts
+++ src/app/dicas-para-o-bolso/dicas-para-o-bolso.component.ts
@@ -26,31 +26,31 @@ export class DicasParaOBolsoComponent {
       titulo: '10 dicas para economizar dinheiro',
       descricao: 'Aprenda a economizar dinheiro',
       imagem: 'https://picsum.photos/id/4/200/150',
-      link: 'https://www.bcb.gov.br/educacaofinanceira/noticias/10-dicas-para-economizar-dinheiro-no-dia-a-dia'
+      link: 'https://www.serasa.com.br/blog/10-dicas-para-economizar-dinheiro/'
     },
     {
-      titulo: '7 passos para sair das dívidas',
+      titulo: '10 passos para sair das dívidas',
       descricao: 'Descubra como sair das dívidas e ficar no azul',
       imagem: 'https://picsum.photos/id/20/200/150',
-      link: 'https://blog.mobills.com.br/7-passos-para-sair-das-dividas/'
+      link: 'https://www.serasa.com.br/limpa-nome-online/blog/10-passos-para-sair-das-dividas/'
     },
     {
       titulo: 'Aprenda a investir com pouco dinheiro',
       descricao: 'Saiba como começar a investir com pouco dinheiro',
       imagem: 'https://picsum.photos/id/48/200/150',
-      link: 'https://blog.nubank.com.br/investir-com-pouco-dinheiro/'
+      link: 'https://neon.com.br/aprenda/investimentos/como-investir-com-pouco-dinheiro/'
     },
     {
       titulo: 'Como fazer um planejamento financeiro',
       descricao: 'Saiba como planejar-se financeiramente',
       imagem: 'https://picsum.photos/id/60/200/150',
-      link: 'https://www.submarinofinanceiro.com.br/planilha-financeira/planejamento-financeiro-eficiente/'
+      link: 'https://www.suno.com.br/guias/planejamento-financeiro/'
     },
     {
       titulo: 'Como organizar suas finanças',
       descricao: 'Aprenda a organizar suas finanças',
       imagem: 'https://picsum.photos/id/180/200/150',
-      link: 'https://exame.com/seu-dinheiro/como-organizar-suas-financas-8-passos-para-ter-mais-controle-do-dinheiro/'
+      link: 'https://melhorplano.net/dinheiro/como-organizar-financas'
     }
   ];
 
@@ -58,20 +58,20 @@ export class DicasParaOBolsoComponent {
     {
       titulo: 'Investimentos para iniciantes',
       descricao: 'Aprenda a investir em renda fixa e variável',
-      imagem: 'https://picsum.photos/200/150',
-      link: 'https://www.youtube.com'
+      imagem: 'https://picsum.photos/id/4/200/150',
+      link: 'https://www.youtube.com/watch?v=VkqQB64xn-A'
     },
     {
       titulo: 'Dicas para economizar dinheiro',
       descricao: 'Dicas simples para economizar dinheiro',
-      imagem: 'https://picsum.photos/200/150',
-      link: 'https://www.youtube.com'
+      imagem: 'https://picsum.photos/id/48/200/150',
+      link: 'https://www.youtube.com/watch?v=6nozjRdiOps'
     },
     {
       titulo: 'Como sair das dívidas',
       descricao: 'Passo a passo para sair das dívidas e organizar as finanças',
-      imagem: 'https://picsum.photos/200/150',
-      link: 'https://www.youtube.com'
+      imagem: 'https://picsum.photos/id/60/200/150',
+      link: 'https://www.youtube.com/watch?v=8zj0GJKTWwE'
     }
   ];
 }
